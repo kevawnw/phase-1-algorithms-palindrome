c2f0f4763faed3ec9c9063c98bb3411b0a8690f3
Kevawn Warren <kevawnw@gmail.com>
2022-03-15 12:46:05 -0400

    commit tree 74f107da1fd0e8cd930b0595770d83b1f7c5f397

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i = 0; i < word.length/2; i++){
+    if(word[i] !== word[word.length -i - 1]){
+      return false
+    } 
+  } return true
 }
 
 /* 
